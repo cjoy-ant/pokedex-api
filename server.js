d14fa8c202ad5884845a1b3723e88bfb77c57576
Christine Antonio <christinejm.antonio@gmail.com>
2021-03-11 13:32:32 -0800

    installed cors

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 require("dotenv").config();
 const express = require("express");
 const morgan = require("morgan");
+const cors = require("cors");
 const POKEDEX = require("./pokedex.json");
 
 const app = express();
@@ -27,6 +28,7 @@ const validTypes = [
 ];
 
 app.use(morgan("dev"));
+app.use(cors());
 app.use(validateBearerToken);
 
 app.get("/types", handleGetTypes);
