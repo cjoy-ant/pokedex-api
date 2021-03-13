2f6d74fa1adebadb186f6c325cdf0fb42eb7ef6c
Christine Antonio <christinejm.antonio@gmail.com>
2021-03-13 12:16:11 -0800

    cleaning up code

--- server.js
+++ server.js
@@ -39,18 +39,12 @@ app.get("/types", handleGetTypes);
 app.get("/pokemon", handleGetPokemon);
 
 function validateBearerToken(req, res, next) {
-  // const bearerToken = req.get("Authorization").split(" ")[1];
   const authToken = req.get("Authorization");
   const apiToken = process.env.API_TOKEN;
-
-  //if (bearerToken !== apiToken) {
-  //  return res.status(401).json({ error: "Unauthorized request" });
-  //}
 
   if (!authToken || authToken.split(" ")[1] !== apiToken) {
     return res.status(401).json({ error: "Unauthorized Request" });
   }
-  // move to the next middleware
   next();
 }
 
@@ -60,47 +54,19 @@ function handleGetTypes(req, res) {
 
 function handleGetPokemon(req, res) {
   const { name, type } = req.query;
-  let results = POKEDEX.pokemon;
+  let response = POKEDEX.pokemon;
 
   if (name) {
-    results = results.filter((pokemon) =>
+    response = response.filter((pokemon) =>
       pokemon.name.toLowerCase().includes(name.toLowerCase())
     );
   }
 
-  // if (type) {
-  // results = results.filter((pokemon) => {
-  // for (i = 0; i < pokemon.type.length; i++) {
-  // pokemon.type[i].toLowerCase();
-  // }
-  // })
-  // .filter((pokemon) => pokemon.type.includes(type.toLowerCase()));
-  // }
-
   if (type) {
-    results = results.filter((pokemon) => pokemon.type.includes(type));
+    response = response.filter((pokemon) => pokemon.type.includes(type));
   }
 
-  res.json(results);
-
-  //let response = POKEDEX.pokemon;
-
-  // // filter our pokemon by name if name query param is present
-  // if (req.query.name) {
-  // response = response.filter((pokemon) =>
-  // // case insensitive searching
-  // pokemon.name.toLowerCase().includes(req.query.name.toLowerCase())
-  // );
-  // }
-
-  // // filter our pokemon by type if type query param is present
-  // if (req.query.type) {
-  // response = response.filter((pokemon) =>
-  // pokemon.type.includes(req.query.type)
-  // );
-  // }
-
-  //res.json(response);
+  res.json(response);
 }
 
 app.listen(PORT, () => {
